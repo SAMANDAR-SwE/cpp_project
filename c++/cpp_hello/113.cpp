5660a6e4e5036d0c08b832d50e61222da3086a7f
Samandar Samandarov <samandarovsamandar24@gmail.com>
2025-11-28 17:02:59 +0500

    113_cpp

--- c++/cpp_hello/113.cpp
+++ c++/cpp_hello/113.cpp
@@ -5,8 +5,18 @@ using namespace std;
 int main() {
     int n_son, count;
     cin >> n_son;
-    double list[n_son]:
-    
-    for (count = 0;)
+    double list[n_son];
 
+    for (count = 0; count < n_son; count++){
+        cin >> list[count];
+    }
+    
+    double negativeSum = 0, negativeCount = 0;
+    for (count = 0; count < n_son; count++){
+        if (list[count] < 0){
+            negativeSum += list[count];
+            negativeCount ++;
+        }
+    }
+    printf("%.2f\n", (negativeSum / negativeCount));
 }
