a7b54f4839d3fa5556c4ea4fcd4eff04c6be785f
Samandar Samandarov <samandarovsamandar24@gmail.com>
2025-12-01 20:04:42 +0500

    MASSIV_EDIT_cpp

--- c++/cpp_hello/130.cpp
+++ c++/cpp_hello/130.cpp
@@ -2,25 +2,4 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    int n_son, count;
-    cin >> n_son;
-    double list[n_son];
-
-    for (count = 0; count < n_son; count++){
-        cin >> list[count];
-    
-    }
-    double max = list[count];
-    for (count = 0; count < n_son; count++){
-        if (list[count] > max){
-            max  = list[count];
-        }
-            
-    }
-    for (count = 0; count < n_son; count++){
-        printf("%.2f\n" , (list[count] / max)); 
-    
-    }
-    
-}
+int main() {}
