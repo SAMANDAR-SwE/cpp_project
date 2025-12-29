5f75cd2883e6cb6d55e3175a71d29d5ad6739f7b
SAMANDAR-SwE <samandarovsamandar24@gmail.com>
2025-12-29 20:11:48 +0500

    113_cpp

--- c++/cpp_hello/104.cpp
+++ c++/cpp_hello/104.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n_son, count;
     cin >> n_son;
-    double list[n_son];
+    vector<int> list(n_son);
 
     for (count = 0; count < n_son; count++){
         cin >> list[count];
@@ -17,14 +17,14 @@ int main() {
         if (list[count] < list[min_idx]){
             min_idx = count;            
         }
+
+    if (n_son > 0) {
+        swap(list[min_idx], list.back());
+    }
     
     }
     for (count = 0; count < n_son; count++){
-        if (list[count] == list[min_idx]){
-            if (count == n_son){
-                list[count] = list[min_idx];
-            }
-        }
+        cout << list[count] << " ";
     
     }
     
